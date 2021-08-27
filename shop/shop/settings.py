ed9bbf9ab99fa05f4430587f302e35f1dbbc860a
bananaderevo <vladikarshinov55@gmail.com>
2021-08-27 09:36:10 +0300

    navbar was added

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop_app.apps.ShopAppConfig',
+
+    'shop',
+    'bootstrap4',
+
 ]
 
 MIDDLEWARE = [
--- shop/shop_app/views.py
+++ shop/shop_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
+
+def book_list(request):
+    data1 = requests.get('http://127.0.0.1:8000/')
+    data = json.loads(data1.text)
+    p = data['results']
+    return render(request, 'shop/main.html', {'data': p})
